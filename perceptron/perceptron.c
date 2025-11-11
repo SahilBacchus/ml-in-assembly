952f0233efcb8dd3348e3e49173da080f737ad34
Sahil <sahilbacchus@gmail.com>
2025-11-11 15:48:01 -0700

    feat(perceptron): add C implementation of  main()

--- perceptron/perceptron.c
+++ perceptron/perceptron.c
@@ -1,10 +1,36 @@
+#include <stdio.h>
+
 #define MAX_FEATURES 2
 
 int predict(double x[], int n, double weights[]);
 int step(double z);
 void train(double X[][MAX_FEATURES], double Y[], int num_samples, int num_features, double weights[], double eta, int epochs);
 
+int main(void){
+    double X[][MAX_FEATURES] = {{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+    double y[] = {0, 0, 0, 1};
+    double weights[MAX_FEATURES + 1]; 
+    int y_pred; 
 
+    train(X, y, 4, 2, weights, 0.1, 5); 
+
+    printf("\n==== Model Stats ====");
+    printf("\nWeights: ["); 
+    for (int i = 0; i < MAX_FEATURES + 1; i++){
+        printf("%lf, ", weights[i]); 
+    }
+    printf("]"); 
+
+    printf("\n\n==== Testing ====");
+    printf("\nPredictions on AND Gate:");
+    for(int i = 0; i < 4; i++){
+        y_pred = predict(X[i], 2, weights); 
+        printf("\n\tx = [%lf, %lf] -> y = %d", X[i][0], X[i][1], y_pred);
+    }
+    printf("\n");
+
+    return 0; 
+}
 
 
 void train(double X[][MAX_FEATURES], double Y[], int num_samples, int num_features, double weights[], double eta, int epochs){
